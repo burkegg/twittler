c81c46753b37db595f3fd638b6d028b06c1cd562
burkegg <burkegreen@gmail.com>
2018-05-13 15:19:18 -0700

    Refactored all javascript to logic2.js.  Everything works perfectly. All minimum requirements met.  1 advanced req. met (timestamp).

--- logic2.js
+++ logic2.js
@@ -8,6 +8,7 @@ $(document).ready(function(){
 	// Add init to history
 
 	let postTweet = function(tweet, home_idx){
+		// Formats and posts a tweet to the page.
 		var $tweet = $('<a class="twit"></a>');
     	var $user = $('<span class = "user"></span>"');
     	var $message = $('<span class = "message"></span>');
@@ -26,6 +27,8 @@ $(document).ready(function(){
 
     // Pre-loop post of initial streams.home.  This should only happen once.
     let postAllTweets = function(current_prefix){
+    	// Input an array of tweets and post them automatically.  This runs at
+    	// first page load, and then again each time the view changes.
 	    if (streams.home.length > 0){
 			let pre_loop_idx = current_prefix.length - 1;
 			for (let idx = 0; idx < pre_loop_idx; idx++){
@@ -37,35 +40,24 @@ $(document).ready(function(){
 	}
     
 	postAllTweets(streams.home);
-    // Interval:   prefix should be "prefix - should change based"
-    // for all new tweets...
-    //.   add to history
-    //.   add to page
+    let counter = 0;
+    let bottom_of_loop_idx = history.length - 1;
+	setInterval(function(){
+		// Each interval should check for new tweets in the current prefix.
+		// It should also update all timestamps for posted tweets.
 
-    
-    	let counter = 0;
-    	let bottom_of_loop_idx = history.length - 1;
-		setInterval(function(){
-  			let top_of_loop_idx = prefix.length - 1;
-  			for (let idx = bottom_of_loop_idx; idx <= top_of_loop_idx; idx++){
-  				postTweet(prefix[idx], idx);
-  			}
-  			bottom_of_loop_idx = top_of_loop_idx;
+  		let top_of_loop_idx = prefix.length - 1;
+  		for (let idx = bottom_of_loop_idx; idx <= top_of_loop_idx; idx++){
+  			postTweet(prefix[idx], idx);
+  		}
+  		bottom_of_loop_idx = top_of_loop_idx;
 
-
-
-  			// for all tweets - don't worry about which prefix - just do all of them.
-    		//.   update time
-    		counter++;
-    		for (let idx = 0; idx < prefix.length; idx++){
-    			//$('.timestamp' + idx.toString()).text(timeSincePosted(streams.home[idx].created_at));
-    		    //$('.timestamp' + idx.toString()).text('my index:  ' + idx + ' counter: ' + counter);
-    		    console.log(idx);
-    		    $('.timestamp' + idx.toString()).text(timeSincePosted(prefix[idx].created_at));
+    	for (let idx = 0; idx < prefix.length; idx++){
+    	$('.timestamp' + idx.toString()).text(timeSincePosted(prefix[idx].created_at));
 			}
+    }, 5000) 
 
-    	}, 5000) // End of interval.
-
+	// A user can click on a tweet to see all of that user's tweets.
 	$(document).on('click', '.twit', function(){
 		watchUser = $(this).find('.user').text();
 		watchUser = watchUser.slice(1, watchUser.length - 1);
@@ -79,7 +71,14 @@ $(document).ready(function(){
 		prefix = streams.users[watchUser];
 	});
 
+	// A user can click on the back button to see all of the tweets.
+	$(document).on('click', '.backButton', function(){
+		$('.main').empty();
+		prefix = streams.home;
+		$('.backButton').remove();
+		postAllTweets(prefix);
+		$('.view').text('Things are getting weird on Twittler.');
+	});
 
 
-
-})
+});
